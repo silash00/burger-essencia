972b4636ec6d530c50aa65081d01e9c21bb1d3e4
Silas Henrique <silash.silva00@gmail.com>
2026-03-07 22:39:29 -0300

    Update BurgerOrder.tsx to remove unused imports and fix linting errors

---
+++ BurgerOrder.tsx
@@ -0,0 +1,13 @@
+import { LazyMotion, domAnimation } from "framer-motion";
+
+const BurgerOrder = () => {
+  return (
+    <LazyMotion features={domAnimation} strict>
+      <div style={{ textAlign: "center", padding: "20px" }}>
+        <h1>Encerramos os pedidos por hoje</h1>
+      </div>
+    </LazyMotion>
+  );
+};
+
+export default BurgerOrder;
